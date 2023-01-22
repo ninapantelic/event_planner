4cd7d5eac938731d465a70a22296e2004968cbf9
Nina Pantelic <nina.pantelic20@gmail.com>
2023-01-22 14:45:47 +0100

    Azurirani novi kontroleri sa metodama

---
+++ app/Http/Controllers/PerformerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Performer;
+use Illuminate\Http\Request;
+
+class PerformerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Performer  $performer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Performer $performer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Performer  $performer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Performer $performer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Performer  $performer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Performer $performer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Performer  $performer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Performer $performer)
+    {
+        //
+    }
+}
