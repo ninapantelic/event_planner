e391cb9a86cf8b2021f6f202744fd83ade0043b1
Nina Pantelic <nina.pantelic20@gmail.com>
2023-01-22 16:43:40 +0100

    Azuriranje performer event controllera

--- app/Http/Controllers/PerformerEventController.php
+++ app/Http/Controllers/PerformerEventController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Resources\EventCollecton;
+use App\Models\Event;
+use App\Models\Performer;
+
 class PerformerEventController extends Controller
 {
     //
+    public function index($performer_id)
+    {
+        $performer = Performer::find($performer_id);
+        if (is_null($performer)) {
+            return response()->json('Performer not found', 404);
+        }
+
+        $events = Event::get()->where('performer_id', $performer_id);
+        if (is_null($events)) {
+            return response()->json('Events not found', 404);
+        }
+
+        return response()->json([
+            'performer' => $performer->name,
+            'events' => new EventCollecton($events)
+        ]);
+    }
 }
