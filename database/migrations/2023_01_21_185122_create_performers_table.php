d2576256e09272f2b93c79401248e533d5f316d7
Nina Pantelic <nina.pantelic20@gmail.com>
2023-01-22 13:56:28 +0100

    Azurirana tabela performers, ime unique

--- database/migrations/2023_01_21_185122_create_performers_table.php
+++ database/migrations/2023_01_21_185122_create_performers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('performers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('performance');
             $table->string('gender');
             $table->timestamps();
