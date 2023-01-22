952e483ab91c00bf9b328e0c208c62c083f8d836
Nina Pantelic <nina.pantelic20@gmail.com>
2023-01-22 16:43:28 +0100

    Azuriranje LocationEvent controllera

--- app/Http/Controllers/LocationEventController.php
+++ app/Http/Controllers/LocationEventController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Resources\EventCollecton;
+use App\Models\Event;
+use App\Models\Location;
+
 class LocationEventController extends Controller
 {
     //
+    public function index($location_id)
+    {
+        $location = Location::find($location_id);
+        if (is_null($location)) {
+            return response()->json('Location not found', 404);
+        }
+
+        $events = Event::get()->where('location_id', $location_id);
+        if (is_null($events)) {
+            return response()->json('Events not found', 404);
+        }
+
+        return response()->json([
+            'location' => $location->name,
+            'events' => new EventCollecton($events)
+        ]);
+    }
 }
