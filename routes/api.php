893da2dc5557f4ebbc4a482b0a3ddfd1db2297ff
Nina Pantelic <nina.pantelic20@gmail.com>
2023-01-23 14:59:36 +0100

    Api rute napravljene

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\LocationEventController;
+use App\Http\Controllers\PerformerController;
+use App\Http\Controllers\PerformerEventController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +23,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/performers', [PerformerController::class, 'index']);
+Route::get('/performers/{id}', [PerformerController::class, 'show']);
+
+Route::get('/locations', [LocationController::class, 'index']);
+Route::get('/locations/{id}', [LocationController::class, 'show']);
+
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+
+Route::resource('performers.events', PerformerEventController::class)
+    ->only(['index']);
+
+Route::resource('locations.events', LocationEventController::class)
+    ->only(['index']);
+
+
+Route::post('/register', [AuthorCorontroller::class, 'register']);
+Route::post('/login', [AuthorController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('/performers', PerformerController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/locations', LocationController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/events', EventController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::post('/logout', [AuthorController::class, 'logout']);
 });
